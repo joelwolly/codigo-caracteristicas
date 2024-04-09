d614b698045560be2540d996350b300ece65a7c1
joel santos <cjoelsantos90@gmail.com>
2024-04-08 22:48:06 -0300

    Seu mensagem de commit

---
+++ main.py
@@ -0,0 +1,13 @@
+nome = 'joel wollace'
+sobrenome = 'santos'
+idade = 22
+ano_nascimento = 2001 
+maior_de_idade = idade >= 18 - idade
+altura_metros = 170
+
+print('nome:', nome)
+print('sobrenome', sobrenome)
+print('idade', idade)
+print('ano de nascimento:', ano_nascimento)
+print('E maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
